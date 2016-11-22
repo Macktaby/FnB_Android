c07276aab1c5a267b45b312a096ce11de69f1700
ShadyShalaby <shady.shalaby1994@gmail.com>
2016-11-22 16:34:18 +0200

    InitialCommit

---
+++ app/src/main/java/com/macktaby/fnb/view/HomeActivity.java
@@ -0,0 +1,15 @@
+package com.macktaby.fnb.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.macktaby.fnb.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
